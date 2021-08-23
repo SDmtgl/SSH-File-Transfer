ffa5980ff45289de740445cfde5a1005fcfc03cd
Sena <89255611+SDmtgl@users.noreply.github.com>
2021-08-23 11:19:29 +0300

    Update ssh_transfer-server.py

--- ssh_transfer-server.py
+++ ssh_transfer-server.py
@@ -4,15 +4,15 @@ import paramiko
 sshClient = paramiko.SSHClient()
 sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 sshClient.load_system_host_keys()
-sshClient.connect('192.168.106.128',22,'dmtgl','03941998.')
+sshClient.connect('192.168.106.128',22,'dmtgl','Passsword')
 
 ftp_client= sshClient.open_sftp()
 try:
      #Yerelden uzak makineye dosya yükleme
-     #ftp_client.put('C:\\Users\\lenovo\\Desktop\\sil_ubuntu_file.txt','/home/dmtgl/sil_ubuntu_file')
+     ftp_client.put('C:\\Users\\lenovo\\Desktop\\subuntu_file.txt','/home/dmtgl/ubuntu_file')
 
      #Uzak makineden dosya indirme
-     #ftp_client.get('/home/dmtgl/sil_ubuntu_file','C:/Users/lenovo/Desktop/sil_ubuntu.txt',None)
+     ftp_client.get('/home/dmtgl/subuntu_file','C:/Users/lenovo/Desktop/ubuntu.txt',None)
 except Exception as e:
     print(str(e))
 ftp_client.close()
